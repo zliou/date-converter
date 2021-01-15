ef0961598c41a3b1e87451a75faf3f73c4e0be3f
Zachary Liou <zachliou@gmail.com>
2021-01-14 20:35:00 -0800

    Remove hello world test.

--- date.test.js
+++ date.test.js
@@ -1,9 +1,5 @@
 // const date = require('./date');
 
-test('hello world', () => {
-  expect(sum(1,2)).toBe(3);
-});
-
 test("Unix timestamps convert to locale strings.", () => {
   var kTestTimestamp = 1610684094792;
   var kExpectedLocaleString = "1/14/2021, 8:14:54 PM";
